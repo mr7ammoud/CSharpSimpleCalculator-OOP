1d299fac78d38f37ae1283f306a1ed05254f9209
mr7ammoud <mohammed.hammoud12.2003@gmail.com>
2026-01-07 22:46:50 +0300

    Initial commit - Calculator project

---
+++ src/Calculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Project
+{
+    internal class Calculator
+    {
+        private float _Result = 0;
+        private float _LastNumber = 0;
+        private string _FinalOperation = "No Thing";
+
+        private bool _IsZero(float Number)
+        {
+            return (Number == 0);
+        }
+
+        public void Clear()
+        {
+            _Result = 0;
+            _LastNumber = 0;
+            _FinalOperation = "Clear";
+        }
+
+        public void Add(float Number)
+        {
+            _Result += Number;
+            _LastNumber = Number;
+            _FinalOperation = "Adding";
+        }
+
+        public void Subtract(float Number)
+        {
+            _Result -= Number;
+            _LastNumber = Number;
+            _FinalOperation = "Subtracting";
+        }
+
+        public bool Divide(float Number)
+        {
+            bool Succeeded = true;
+            _FinalOperation = "Dividing";
+            if (_IsZero(Number))
+            {
+                _LastNumber = Number;
+                _Result /= 1;
+                Succeeded = false;
+            }
+            else
+            {
+                _LastNumber = Number;
+                _Result /= Number;
+            }
+            return Succeeded;
+
+        }
+
+        public void Multiply(float Number)
+        {
+            _Result *= Number;
+            _LastNumber = Number;
+            _FinalOperation = "Multiplying";
+        }
+
+        public float GetFinalResult()
+        {
+            return _Result;
+        }
+
+        public void PrintResult()
+        {
+            Console.WriteLine("Result after {0} {1} is: {2}", _FinalOperation, _LastNumber, _Result);
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator Calculator1 = new Calculator();
+
+            Calculator1.Clear();
+
+            Calculator1.Add(10);
+            Calculator1.PrintResult();
+
+            Calculator1.Add(100);
+            Calculator1.PrintResult();
+
+            Calculator1.Subtract(20);
+            Calculator1.PrintResult();
+
+            Calculator1.Divide(0);
+            Calculator1.PrintResult();
+
+            Calculator1.Divide(2);
+            Calculator1.PrintResult();
+
+            Calculator1.Multiply(3);
+            Calculator1.PrintResult();
+
+            Calculator1.Clear();
+            Calculator1.PrintResult();
+        }
+    }
+}
